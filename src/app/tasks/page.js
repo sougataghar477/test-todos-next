bd2a61e1ac5512abb04043dc928607b78d0052aa
sougata <sougataghar47@gmail.com>
2024-03-25 21:59:14 +0530

    added key

--- src/app/tasks/page.js
+++ src/app/tasks/page.js
@@ -8,7 +8,7 @@ function Tasks(){
     },[])
 return <Container maw={640}>
     <h1>Tasks</h1>
-    {posts && posts.map(post => <h2>{post}</h2>)}
+    {posts && posts.map((post,index) => <h2 key={index}>{post}</h2>)}
 </Container>
 }
 export default Tasks;
