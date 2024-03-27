7471e3b23069da4517bf02489855da56b584624f
sougata <sougataghar47@gmail.com>
2024-03-27 20:00:25 +0530

    loader center

--- src/app/tasks/page.js
+++ src/app/tasks/page.js
@@ -7,14 +7,17 @@ function Tasks(){
     useEffect(()=>{
         fetch('/api/getPosts').then(a => { return a.json()}).then(b => {setLoading(false);setPosts(b.posts)})
     },[])
-return <Container maw={640}>
-<h1>Tasks</h1>
-{isLoading && (
-   <Center w={'100vw'} h={'100vh'}>
-    <Loader color="blue" />
-   </Center>
-)}
-{posts && posts.map((post,index) => <h2 key={index}>{post}</h2>)}
-</Container>
+    return isLoading ? (
+        <Center w={'100vw'} h={'100vh'}>
+          <Loader color="blue" />
+        </Center>
+      ) : (
+        <Container maxWidth={640}>
+          <h1>Tasks</h1>
+          {posts && posts.map((post, index) => <h2 key={index}>{post}</h2>)}
+        </Container>
+      );
+      
+
 }
 export default Tasks;
