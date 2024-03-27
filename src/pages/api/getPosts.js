513e67551370e6ca372f16a44d4660cb07d1f37b
sougata <sougataghar47@gmail.com>
2024-03-27 13:01:36 +0530

    added getserversession

--- src/pages/api/getPosts.js
+++ src/pages/api/getPosts.js
@@ -1,7 +1,8 @@
 import db from "../../../utils/mongo";
-import { getSession } from 'next-auth/react';
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./auth/[...nextauth]";
 export default async function handler(req, res) {
- const session = await getSession({ req });
+ const session = await getServerSession(req, res, authOptions);
  let foundEmail=await db.collection('users').findOne({email:session.user.email})
  console.log(foundEmail);
  res.status(200).json({posts:foundEmail.posts})
