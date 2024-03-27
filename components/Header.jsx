d85f406fdd32b96e251af7376305a9e5cbf4f035
sougata <sougataghar47@gmail.com>
2024-03-27 14:11:17 +0530

    added avatar

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ return <header>
         <Link href={'/tasks'}>Tasks</Link>
         {status==='authenticated' ? null :<Link href={'/login'}>Login</Link>}
         {status==='authenticated'?<a onClick={() => signOut()}>Log Out</a>:null}
-         
+         <Avatar src={data.user.image}/>
     </Container>
 </header>
 }
