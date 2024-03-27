9d04b42a9b48a0f48f0819ced5e4e81a4583d045
sougata <sougataghar47@gmail.com>
2024-03-27 14:22:28 +0530

    added avatar in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Container,Text } from "@mantine/core";
+import { Avatar, Container,Flex } from "@mantine/core";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react"
@@ -7,12 +7,15 @@ let { data, status } = useSession();
 console.log(data)
 return <header>
     <Container w={640}>
+        <Flex>
         <Link href={'/'}>Home</Link>
         <Link href={'/tasks'}>Tasks</Link>
         {status==='authenticated' ? null :<Link href={'/login'}>Login</Link>}
         {status==='authenticated'?<a onClick={() => signOut()}>Log Out</a>:null}
          <Avatar src={data?.user?.image}/>
-        <Text>{data?.user?.email}</Text>
+        <Text >{data?.user?.email}</Text>
+        </Flex>
+        
     </Container>
 </header>
 }
