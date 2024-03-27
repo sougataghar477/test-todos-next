1a336fbed386d86f4dffee00055842609bc6951e
sougata <sougataghar47@gmail.com>
2024-03-27 17:37:49 +0530

    fixed

--- src/app/tasks/page.js
+++ src/app/tasks/page.js
@@ -1,13 +1,15 @@
 'use client';
 import { useEffect,useState } from "react";
-import { Container } from "@mantine/core";
+import { Container,Loader } from "@mantine/core";
 function Tasks(){
     let [posts,setPosts]=useState([]);
+    let [isLoading,setLoading]=useState(false);
     useEffect(()=>{
-        fetch('/api/getPosts').then(a => a.json()).then(b => setPosts(b.posts))
+        fetch('/api/getPosts').then(a => {setLoading(true); return a.json()}).then(b => {setLoading(false);setPosts(b.posts)})
     },[])
 return <Container maw={640}>
     <h1>Tasks</h1>
+    {isLoading?<Loader color="blue" />:null}
     {posts && posts.map((post,index) => <h2 key={index}>{post}</h2>)}
 </Container>
 }
