80b8451d32537a357fdf38759ade0cd10cf53f58
sougata <sougataghar47@gmail.com>
2024-03-27 16:05:07 +0530

    fixed

--- src/pages/api/signin.js
+++ src/pages/api/signin.js
@@ -1,16 +1,17 @@
 import db from '../../../utils/mongo';
-import { getServerSession } from 'next-auth/react';
+import { getSession } from "next-auth/react"
 import { authOptions } from './auth/[...nextauth]';
 export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-  console.log(session)
-// let foundEmail=await db.collection("users").findOne({email:session?.user?.email});
-// if(foundEmail){
-//   res.json({msg:'email already exists'})
-// }
-//     else{
-//       await db.collection("users").insertOne({email:session.user.email,posts:[]});
-//       res.json({msg:'Email added'})
-//     }
-      res.json({msg:'hello'})
+  const session = await getSession(req);
+  console.log(session);
+  if(session){
+    let foundEmail=await db.collection("users").findOne({email:session?.user?.email});
+    if(foundEmail){
+      res.json({msg:'email already exists'})
     }
+        else{
+          await db.collection("users").insertOne({email:session.user.email,posts:[]});
+          res.json({msg:'Email added'})
+        }
+    }
+  }
