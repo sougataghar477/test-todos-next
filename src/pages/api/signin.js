5272771eff2b7a2c96321ba2b9dc3cc79bd7568f
sougata <sougataghar47@gmail.com>
2024-03-27 15:46:46 +0530

    fixed

--- src/pages/api/signin.js
+++ src/pages/api/signin.js
@@ -4,13 +4,13 @@ import { authOptions } from './auth/[...nextauth]';
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions);
   console.log(session)
-let foundEmail=await db.collection("users").findOne({email:session?.user?.email});
-if(foundEmail){
-  res.json({msg:'email already exists'})
-}
-    else{
-      await db.collection("users").insertOne({email:session.user.email,posts:[]});
-      res.json({msg:'Email added'})
-    }
-      
+// let foundEmail=await db.collection("users").findOne({email:session?.user?.email});
+// if(foundEmail){
+//   res.json({msg:'email already exists'})
+// }
+//     else{
+//       await db.collection("users").insertOne({email:session.user.email,posts:[]});
+//       res.json({msg:'Email added'})
+//     }
+      res.json(session)
     }
